285668c42f430a96ee83c35da51afd035f1c1bdd
Mark Ashton <meashton@gmail.com>
2025-12-18 15:48:32 -0600

    day 2 part 2

--- day2/solution.py
+++ day2/solution.py
@@ -1,6 +1,6 @@
 import sys
 
-def is_valid(id):
+def is_valid_part1(id):
     id = str(id)
     num_digits = len(id)
     if num_digits % 2 != 0:
@@ -9,21 +9,43 @@ def is_valid(id):
     second_half = id[(num_digits//2):]
     return first_half != second_half
 
+def is_valid_part2(id):
+    id = str(id)
+    num_digits = len(id)
+    for sub_length in range(1, num_digits):
+        if num_digits % sub_length != 0:
+            continue
+        subsequences = []
+        start = 0
+        end = sub_length
+        while end <= num_digits:
+            subsequences.append(id[start:end])
+            start += sub_length
+            end += sub_length
+        if all(map(lambda x: x == subsequences[0], subsequences[1:])):
+            return False
+    return True
+
+
 def part1():
     invalid_id_sum = 0
     with open('input.txt') as f:
         input = f.read()
         for ids in input.split(','):
             start, end = map(int, ids.strip().split('-'))
-            invalid_ids = filter(lambda x: not is_valid(x), range(start, end + 1))
+            invalid_ids = filter(lambda x: not is_valid_part1(x), range(start, end + 1))
             invalid_id_sum += sum(invalid_ids)
     return invalid_id_sum
 
-
-
 def part2():
+    invalid_id_sum = 0
     with open('input.txt') as f:
-        pass
+        input = f.read()
+        for ids in input.split(','):
+            start, end = map(int, ids.strip().split('-'))
+            invalid_ids = filter(lambda x: not is_valid_part2(x), range(start, end + 1))
+            invalid_id_sum += sum(invalid_ids)
+    return invalid_id_sum
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == '2':
--- day2/solution_test.py
+++ day2/solution_test.py
@@ -2,10 +2,16 @@ import unittest
 import solution
 
 class SolutionTest(unittest.TestCase):
-    def test_is_valid(self):
-        self.assertFalse(solution.is_valid(1010))
-        self.assertTrue(solution.is_valid(101))
-        self.assertTrue(solution.is_valid(1110))
+    def test_is_valid_part1(self):
+        self.assertFalse(solution.is_valid_part1(1010))
+        self.assertTrue(solution.is_valid_part1(101))
+        self.assertTrue(solution.is_valid_part1(1110))
+
+    def test_is_valid_part2(self):
+        self.assertFalse(solution.is_valid_part2(1010101010))
+        self.assertFalse(solution.is_valid_part2(123123123))
+        self.assertFalse(solution.is_valid_part2(11))
+        self.assertTrue(solution.is_valid_part2(123))
 
 if __name__ == '__main__':
     unittest.main()
