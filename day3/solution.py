5f6e423de6fcca62d73c3529bddea079e4c96a08
Mark Ashton <meashton@gmail.com>
2025-12-19 12:12:05 -0600

    Day 3 part 2

--- day3/solution.py
+++ day3/solution.py
@@ -1,18 +1,20 @@
 import sys
 from functools import reduce
 
-def max_joltage(battery):
-    most_significant_digit = -1
-    least_significant_digit = -1
-    sig_dig_pos = 0
-    for i, c in enumerate(battery[0:-1]):
-        if int(c) > most_significant_digit:
-            most_significant_digit = int(c)
-            sig_dig_pos = i
-    for c in battery[(sig_dig_pos + 1):]:
-        if int(c) > least_significant_digit:
-            least_significant_digit = int(c)
-    return (10 * most_significant_digit) + least_significant_digit
+def max_joltage(battery, length = 2):
+    digits = []
+    start = 0
+    remaining = length
+    end = len(battery) - remaining + 1
+    while remaining > 0:
+        max_digit = max(battery[start:end])
+        position = battery[start:end].index(max_digit) + start
+        digits.append(max_digit)
+        remaining -= 1
+
+        end = len(battery) - remaining + 1
+        start = position + 1
+    return int(''.join(map(str, digits)))
 
 def part1():
     with open('input.txt') as f:
@@ -20,8 +22,11 @@ def part1():
         return sum(maxes)
 
 
+
 def part2():
-    pass
+    with open('input.txt') as f:
+        maxes = map(lambda x: max_joltage(x.strip(), 12), f)
+        return sum(maxes)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == '2':
--- day3/solution_test.py
+++ day3/solution_test.py
@@ -8,5 +8,17 @@ class SolutionTest(unittest.TestCase):
     def test_max_joltage_most_sig_dig_is_last(self):
         self.assertEqual(solution.max_joltage('123459'), 59)
 
+    def test_max_joltage_sample_values(self):
+        self.assertEqual(solution.max_joltage('987654321111111'), 98)
+        self.assertEqual(solution.max_joltage('811111111111119'), 89)
+        self.assertEqual(solution.max_joltage('234234234234278'), 78)
+        self.assertEqual(solution.max_joltage('818181911112111'), 92)
+
+    def test_max_joltage_with_higher_lengths(self):
+        self.assertEqual(solution.max_joltage('987654321111111', 12), 987654321111)
+        self.assertEqual(solution.max_joltage('811111111111119', 12), 811111111119)
+        self.assertEqual(solution.max_joltage('234234234234278', 12), 434234234278)
+        self.assertEqual(solution.max_joltage('818181911112111', 12), 888911112111)
+
 if __name__ == '__main__':
     unittest.main()
