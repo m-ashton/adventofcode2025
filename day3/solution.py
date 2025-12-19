62a415f55f6c78e44b4c4963070904f8df868814
Mark Ashton <meashton@gmail.com>
2025-12-19 12:24:50 -0600

    a little cleanup

--- day3/solution.py
+++ day3/solution.py
@@ -10,18 +10,16 @@ def max_joltage(battery, length = 2):
         max_digit = max(battery[start:end])
         position = battery[start:end].index(max_digit) + start
         digits.append(max_digit)
-        remaining -= 1
 
-        end = len(battery) - remaining + 1
+        remaining -= 1
         start = position + 1
+        end = len(battery) - remaining + 1
     return int(''.join(map(str, digits)))
 
 def part1():
     with open('input.txt') as f:
         maxes = map(lambda x: max_joltage(x.strip()), f)
         return sum(maxes)
-
-
 
 def part2():
     with open('input.txt') as f:
